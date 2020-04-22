533e9017bb32b46f3fe5662e0a30ccc14fba89fd
Nate Lovallo <NateLovallo@users.noreply.github.com>
2020-04-21 21:41:00 -0600

    Trevor's patch and FPS counter

--- test.cpp
+++ test.cpp
@@ -29,21 +29,15 @@ int main(int argc, char* args[])
    }
 
    /////////////////////////////////////////////////////////////////////
-   SDL_Texture* myTex = NULL;
-
-   int stride= 400 * 4;
-   unsigned char *image = (unsigned char*)calloc(stride*400, 1);
-   Uint32 rmask, gmask, bmask, amask;
-
-   rmask = 0x00ff0000;
-   gmask = 0x0000ff00;
-   bmask = 0x000000ff;
-   amask = 0xff000000;
+   SDL_Texture *myTex = SDL_CreateTexture(myRenderer, SDL_PIXELFORMAT_RGBA32, 
+      SDL_TEXTUREACCESS_STREAMING, 400, 400);
 
    double rotate = 0.0;
    
    /////////////////////////////////////////////////////////////////////
    bool run = true;
+   
+   uint32_t lastTicks = 0;
    
    while (run)
    {
@@ -70,57 +64,59 @@ int main(int argc, char* args[])
       
       // draw an arc in cairo
       {
-         cairo_surface_t *cairo_surface;
-         
-         cairo_surface = cairo_image_surface_create_for_data (image,
-               CAIRO_FORMAT_ARGB32,
-               400, 400, stride);
 
-         cairo_t *cr=cairo_create(cairo_surface);
-         
-         // probably dont need this
-         // wait wtf i do
-         cairo_set_source_rgb (cr, 0, 0, 0);
-         cairo_paint (cr);
-         
-         cairo_set_source_rgba (cr, 0, 1, 0, 0.5);
-         cairo_set_line_width (cr, 10.0);
-         cairo_arc (cr, 200, 200, 100, (rotate)*(M_PI/180.0), (rotate+270.0)*(M_PI/180.0));
-         cairo_stroke (cr);
+         void *pixels;
+         int pitch;
+         if (0 == SDL_LockTexture(myTex, NULL, &pixels, &pitch))
+         {
+            cairo_surface_t *cairo_surface =
+               cairo_image_surface_create_for_data ((unsigned char*)pixels, CAIRO_FORMAT_ARGB32, 400, 400, pitch);
+            cairo_t *cr=cairo_create(cairo_surface);
 
-         cairo_surface_destroy (cairo_surface);
-         cairo_destroy (cr);
+            // probably dont need this
+            // wait wtf i do
+            cairo_set_source_rgb (cr, 0, 0, 0);
+            cairo_paint (cr);
+
+            cairo_set_source_rgba (cr, 0, 1, 0, 0.5);
+            cairo_set_line_width (cr, 10.0);
+            cairo_arc (cr, 200, 200, 100, (rotate)*(M_PI/180.0), (rotate+270.0)*(M_PI/180.0));
+            cairo_stroke (cr);
+            
+            
+            cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+            cairo_set_font_size (cr, 30.0);
+            cairo_move_to (cr, 150.0, 350.0);
+            
+            uint32_t ticks = SDL_GetTicks();
+            uint32_t delta = ticks-lastTicks;
+            char sFps[32];
+            double fps = 1000.0 / (double)delta;
+            
+            sprintf(sFps, "%3.0f fps", fps);
+            cairo_show_text (cr, sFps);
+            
+            lastTicks = ticks;
+
+            cairo_surface_destroy (cairo_surface);
+            cairo_destroy (cr);
+
+            SDL_UnlockTexture(myTex);
+         }
       }
-      
-      // render the cairo data to a texture
-      {
-         SDL_Surface* img = SDL_CreateRGBSurfaceFrom( (void *) image, 400, 400, 32, stride, rmask, gmask, bmask, amask);
-         SDL_Surface* formattedSurface = SDL_ConvertSurfaceFormat( img, SDL_PIXELFORMAT_RGBA8888, 0 );
-         SDL_FreeSurface( img );
-
-         myTex = SDL_CreateTextureFromSurface( myRenderer, formattedSurface );
-         
-         SDL_FreeSurface( formattedSurface );
-      }
-
-
+  
       // draw to screen
       SDL_RenderCopy( myRenderer, myTex, NULL, NULL);
       
-      
       // flip buffer
       SDL_RenderPresent(myRenderer);
-      
-      SDL_DestroyTexture( myTex );
-      
-      memset(image, 0, sizeof(image));
       
       rotate += 1.0;
       if (rotate >= 360.0)
       {
          rotate = 0.0;
       }
-      
+            
       // TODO frame timer?
       SDL_Delay(20);
    }
